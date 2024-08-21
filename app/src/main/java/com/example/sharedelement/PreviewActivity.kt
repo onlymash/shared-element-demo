52b0bd2a61f3197af4529ff799659244162f79e7
onlymash <fiepi.dev@gmail.com>
2024-08-21 19:29:24 +0800

    Fix typo

--- app/src/main/java/com/example/sharedelement/PreviewActivity.kt
+++ app/src/main/java/com/example/sharedelement/PreviewActivity.kt
@@ -2,13 +2,16 @@ package com.example.sharedelement
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.annotation.OptIn
 import androidx.core.app.ActivityOptionsCompat
+import androidx.media3.common.util.UnstableApi
 import com.example.sharedelement.databinding.ActivityPreviewBinding
 
 class PreviewActivity : BaseActivity<ActivityPreviewBinding>() {
 
 	override val binding by lazy { ActivityPreviewBinding.inflate(layoutInflater) }
 
+	@OptIn(UnstableApi::class)
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding.ivPreview.setOnClickListener {
